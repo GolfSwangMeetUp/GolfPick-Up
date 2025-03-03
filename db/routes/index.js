c00bd6282cce346d1e425d632084d75eddaec908
GolfSwangMeetUp <dria2thomas@gmail.com>
2025-03-02 18:29:19 -0600

    Delete db/routes directory

--- db/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-/* GET home page */
-router.get("/", (req, res, next) => {
-  console.log(req.headers);
-  res.json({ message: "test" });
-});
-
-module.exports = router;
